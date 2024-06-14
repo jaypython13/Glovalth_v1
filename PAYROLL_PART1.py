5cbc8ffaefe741485cfcc7b08e9c7328cac5fb5a
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-14 12:00:27 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -19,9 +19,8 @@ if choice == "Employee Work Management Portal":
        		st.write("""### Check Your Timesheet allocation here""")
         	st.write(data[["Employee ID",'Location','Date','Day','Shift Timing','Tasks']])
         	Date = st.selectbox("Select the date", df.columns)
-        	 
-    	else:
-        	st.write("Employee number not found. Please contact your organisation")
+        else:
+		st.write("Employee number not found. Please contact your organisation")
                 
 else:
     st.write("Our Business team ensures smooth and efficient operations, supporting every aspect of our business. From handling client inquiries to managing internal processes, they are the backbone of our organization. Their dedication to excellence and attention to detail guarantee high-quality service and operational integrity.")
