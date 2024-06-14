bb229fea7b83ba82e9813c6c4c429874b06ceb98
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-14 11:13:31 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -10,10 +10,9 @@ emp_number = st.text_input("Enter Employee ID")
 
 df1 = pd.read_csv('E001_EMP_DATA.csv',usecols = ['Location', 'Date','Day', 'Shift Timing','Tasks'])
 df2 = pd.read_csv('E012_EMP_DATA.csv',usecols = ['Location', 'Date','Day', 'Shift Timing','Tasks'])
-])
 
 #df = pd.concat(map(pd.read_csv, ['Feb_Data.csv','March_Data.csv']))
-#df =pd.concat(df1, df2)
+df =pd.concat(df1, df2)
 #def netpay_diff(emp_number):    
     #netpay_diff = df1["Net Pay"]-df2["Net Pay"]
     #st.write(netpay_diff)
