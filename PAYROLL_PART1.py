2cd5144c81a2c430d0f177ae51aaef490263659d
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-14 12:04:56 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -23,5 +23,6 @@ if choice == "Employee Work Management Portal":
 	   st.write("Employee number not found. Please contact your organisation")
                 
 else:
-    st.write(""" ### Our Business team ensures smooth and efficient operations, supporting every aspect of our business. From handling client inquiries to managing internal processes, they are the backbone of our organization. Their dedication to excellence and attention to detail guarantee high-quality service and operational integrity.""")
+	st.subheader(" What we do")
+	st.info(""" ### Our Business team ensures smooth and efficient operations, supporting every aspect of our business. From handling client inquiries to managing internal processes, they are the backbone of our organization. Their dedication to excellence and attention to detail guarantee high-quality service and operational integrity.""")
 
