14dac6f84531d36ec0b0d5879fc6a765457160d1
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-14 10:32:28 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 
 # Streamlit User Interface part
-st.set_page_config(page_title ="GlobalHRIS", page_icon =":guardsman:", layout ="wide")
+st.set_page_config(page_title ="Glovalth", page_icon =":guardsman:", layout ="wide")
 st.image("logo.png", width = 400)
-st.title("Global HR Implementation Services Limited \n AI Employee Net Pay Difference Calculator")
-emp_number = st.text_input("Enter Employee Number")
+st.title("Glovalth Health private Limited \n Care Home EmployeeTime Sheet Management")
+emp_number = st.text_input("Enter Employee ID")
 
-df1 = pd.read_csv('Feb_Data.csv', usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
-df2 = pd.read_csv('March_Data.csv',usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
+df1 = pd.read_csv('E001_Emp_Data.csv', usecols = ['Employee ID', 'Employee Name', 'Organisation ID'])
+df2 = pd.read_csv('E012_Emp_Data.csv',usecols = ['Employee ID', 'Employee Name', 'Organisation ID'])
 
 #df = pd.concat(map(pd.read_csv, ['Feb_Data.csv','March_Data.csv']))
 df =pd.concat(df1, df2)
@@ -19,18 +19,18 @@ df =pd.concat(df1, df2)
 
 # Condition checking              
 if emp_number:
-    feb = df1[df1['Employee Number'] == int(emp_number)]
-    Mar = df2[df2['Employee Number'] == int(emp_number)]
-    st.write("""### February employee data""")
-    st.write(feb)
-    st.write("""### March employee data""")
-    st.write(Mar)
-    current_month_salary = int(row['Net Pay March'])
-    previous_month_salary = int(row['Net Pay Feb'])
-    difference = current_month_salary - previous_month_salary
-    st.write("The net pay difference for employee number {} is:".format(employee_number), difference)  
+    e001 = df1[df1['Employee ID'] == int(emp_number)]
+    e012 = df2[df2['Employee ID'] == int(emp_number)]
+    st.write("""### E001 employee data""")
+    st.write(e001)
+    st.write("""### E012 employee data""")
+    st.write(e012)
+    #current_month_salary = int(row['Net Pay March'])
+    #previous_month_salary = int(row['Net Pay Feb'])
+    #difference = current_month_salary - previous_month_salary
+    st.write("The workplan for employee number {} is:".format(), difference)  
 else:
-    st.write("Employee number not found in the CSV file.")
+    st.write("Employee number not found. Please contact your organisation")
                 
     
 
