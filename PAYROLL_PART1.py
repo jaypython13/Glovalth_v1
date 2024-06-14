eabb008b88a5de00812d5ad00fda0c3e232cae15
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-14 11:33:21 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -24,9 +24,9 @@ df = pd.concat(map(pd.read_csv, ['E001_EMP_DATA.csv','E012_EMP_DATA.csv']))
 
 # Condition checking              
 if emp_number:
-        if df[df['Employee ID'] == emp_number]:
-            st.write("""### Check Your Timesheet allocation here""")
-            st.write(df)
+        df[df['Employee ID'] == emp_number]:
+        st.write("""### Check Your Timesheet allocation here""")
+        st.write(df)
     #E012 = df[df['Employee ID'] == emp_number]
     #st.write("""### Check Your Timesheet allocation here""")
     #st.write(E012)
