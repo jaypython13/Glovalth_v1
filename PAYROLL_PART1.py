15605552c2fa570d2b6b128464c8e8d401c2ace3
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-14 12:17:57 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -5,7 +5,7 @@ import pandas as pd
 # Streamlit User Interface part
 st.set_page_config(page_title ="Glovalth", page_icon =":guardsman:", layout ="wide")
 st.image("Glovalth_logo.jpeg", width = 400)
-st.title("Glovalth Health Tech Care")
+st.title("Glovalth Health Tech Care Limited")
 menu = ["About Us","Employee Work Management Portal"]
 choice = st.sidebar.selectbox("Menu",menu)
 
@@ -18,12 +18,13 @@ if choice == "Employee Work Management Portal":
         	data = df[df['Employee ID'] == emp_number]
        		st.write("""## Check Your Timesheet allocation here""")
         	st.write(data[["Employee ID",'Location','Date','Day','Shift Timing','Tasks']])
-		option = st.select
+		option = st.selectbox("Choose Here", data.columns)
+		st.write(data.option)
 		
 	else:
 	   st.write("""##### If you dont know your Employee number or work is not allocated, Please contact your organisation""")
                 
 else:
-	st.subheader(" What we do")
+	st.subheader("What We Do")
 	st.info(""" ### Our Business team ensures smooth and efficient operations, supporting every aspect of our business. From handling client inquiries to managing internal processes, they are the backbone of our organization. Their dedication to excellence and attention to detail guarantee high-quality service and operational integrity.""")
 
