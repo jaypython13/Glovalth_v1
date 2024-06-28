4da48f8a33e0684882f6fdc6e52b85ce54568bf6
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-28 18:04:26 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -7,20 +7,6 @@ from pandas.api.types import (
     is_object_dtype,
 )
 
-#def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    #"""
-    #Adds a UI on top of a dataframe to let viewers filter columns
-
-    #Args:
-        #df (pd.DataFrame): Original dataframe
-
-    #Returns:
-        #pd.DataFrame: Filtered dataframe
-    #"""
-    #modify = st.checkbox("Add filters")
-
-    #if not modify:
-        #return df 
 
 #df1 = pd.read_csv('E001_EMP_DATA.csv',usecols = ['Employee ID','Location', 'Date','Day', 'Shift Timing','Tasks'])#df2 = pd.read_csv('E012_EMP_DATA.csv',usecols = ['Employee ID','Location', 'Date','Day', 'Shift Timing','Tasks'])
 
@@ -38,61 +24,7 @@ if choice == "Employee Work Management Portal":
 	df = pd.concat(map(pd.read_csv, ['E001_EMP_DATA.csv','E012_EMP_DATA.csv']))   
 	if emp_number:
 		data = df[df['Employee ID'] == emp_number]
-		for col in data.columns:
-			if is_object_dtype(data[col]):
-    				try:
-    	    				data[col] = pd.to_datetime(data[col])
-    				except Exception:
-    	    				pass
-			if is_datetime64_any_dtype(data[col]):
-        			data[col] = data[col].dt.tz_localize(None)
-
 		
-		modification_container = st.container()
-		with modification_container:
-        		to_filter_columns = st.multiselect("Filter dataframe on", data.columns)
-        		for column in to_filter_columns:
-            			left, right = st.columns((1, 20))
-            			# Treat columns with < 10 unique values as categorical
-            			if is_categorical_dtype(data[column]) or data[column].nunique() < 10:
-                			user_cat_input = right.multiselect(
-                   				 f"Values for {column}",
-                    				 data[column].unique(),
-                    				 default=list(data[column].unique()),
-					) 
-					
-					data = data[data[column].isin(user_cat_input)]
-					
-				elif is_numeric_dtype(data[column]):
-                			_min = float(data[column].min())
-               	 			_max = float(data[column].max())
-                			step = (_max - _min) / 100
-                			user_num_input = right.slider(
-                    				f"Values for {column}",
-                    				min_value=_min,
-                    				max_value=_max,
-                    				value=(_min, _max),
-                    				step=step,
-					)
-					data = data[data[column].between(*user_num_input)]
-            			elif is_datetime64_any_dtype(data[column]):
-                			user_date_input = right.date_input(
-                    				f"Values for {column}",
-                    				value=(
-                        				data[column].min(),
-                        				data[column].max(),
-                    				),
-                			)
-                			if len(user_date_input) == 2:
-                    				user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                    				start_date, end_date = user_date_input
-                    				data = data.loc[data[column].between(start_date, end_date)]
-            			else:
-               	 			user_text_input = right.text_input(
-                    				f"Substring or regex in {column}",
-                			)
-                			if user_text_input:
-                    				data = data[data[column].astype(str).str.contains(user_text_input)]
 		st.write("""## Check Your Timesheet allocation here""")
         	st.write(data[["Employee ID",'Location','Date','Day','Shift Timing','Tasks']])
     
