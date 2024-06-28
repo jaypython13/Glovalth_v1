e0e8d26d17a2fe06e6f2bda9ac03f85e22a543eb
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-28 19:41:45 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -28,11 +28,11 @@ if choice == "Employee Work Management Portal":
 		st.write("""## Check Your Timesheet allocation here""")
 		
 		search_date = data.Date.unique().tolist()
-		search_time = data.Shift_Timing.unique().tolist()
+		search_time = data.ShiftTiming.unique().tolist()
 		choices = st.multiselect(" ",search_date)
 		st.write(data[data.Date.isin(choices)])
 		choices = st.multiselect(" ",search_time)
-		st.write(data[data.Shift_Timing.isin(choices)])
+		st.write(data[data.ShiftTiming.isin(choices)])
 		
 		#st.write(data[["Employee ID",'Location','Date','Day','Shift Timing','Tasks']])
     
