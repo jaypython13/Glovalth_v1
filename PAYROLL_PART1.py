87d04ff304aaea90c8f8b0f9069128f9939942c4
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-29 18:05:06 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -12,7 +12,7 @@ from streamlit_dynamic_filters import DynamicFilters
 st.set_page_config(page_title ="Glovalth", page_icon =":guardsman:", layout ="wide")
 st.sidebar.image("Glovalth_logo.jpeg", width =200)
 
-st.title(" Glovalth Health Tech Care Limited")
+st.title(" Sathish thangam I love you")
 with st.sidebar:
     choice = option_menu("Main Menu", ["Home", "Employee Work Management Portal", "Employee Attendence Form", "Care Planning for Care Home", "Medication Activity", "Document Verification for Care Home", "Emergency", ], 
         icons=['house', 'list-task', 'cloud-upload', 'list-task', 'list-task','list-task'], menu_icon="cast", default_index=1)
