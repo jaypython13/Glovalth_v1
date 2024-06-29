8159c82a0faa9193587a932f6f4fc2b725ad7121
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-06-29 18:09:44 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -12,7 +12,7 @@ from streamlit_dynamic_filters import DynamicFilters
 st.set_page_config(page_title ="Glovalth", page_icon =":guardsman:", layout ="wide")
 st.sidebar.image("Glovalth_logo.jpeg", width =200)
 
-st.title("Glovalth Health Tech Limited")
+
 with st.sidebar:
     choice = option_menu("Main Menu", ["Home", "Employee Work Management Portal", "Employee Attendence Form", "Care Planning for Care Home", "Medication Activity", "Document Verification for Care Home", "Emergency", ], 
         icons=['house', 'list-task', 'cloud-upload', 'list-task', 'list-task','list-task'], menu_icon="cast", default_index=1)
@@ -53,8 +53,9 @@ if choice == "Employee Work Management Portal":
 	   st.write("""##### If you dont know your Employee number or work is not allocated, Please contact your organisation""")
                 
 if choice == "Home":
+	st.title("Glovalth Health Tech Limited")
 	st.subheader ("Who we are")
-	st.info(""" ##### Proudly serving the UK’s care homes from our Leicester headquarters. We specialize in timesheet management solutions tailored for the care industry. Our tools simplify payroll, boost productivity, and ensure compliance. Partner with us for efficient and reliable staff management.""")
+	st.info(""" ##### Proudly serving the UK’s care homes from our Leicester headquarters. We specialize in timesheet management solutions tailored for the care industry. Our tools simplify payroll, boost productivity, and ensure compliance. Partner with us for efficient and reliable staff management.""", colour= 'orange')
 	st.subheader("What We Do")
 	st.info(""" ##### Our Business team ensures smooth and efficient operations, supporting every aspect of our business. From handling client inquiries to managing internal processes, they are the backbone of our organization. Their dedication to excellence and attention to detail guarantee high-quality service and operational integrity. Our primary objective is to develop comprehensive and standardized HR software applications tailored specifically for UK industries. We aim to provide solutions that streamline human resource management, enhance operational efficiency, and support the unique needs of businesses across various sectors in the UK. By leveraging advanced technology and industry best practices, we strive to deliver user-friendly and adaptable software that meets the evolving demands of the modern workforce.""")
 	st.subheader("How we support")
