5d71bc14a1091fdf628994c5df260e32205ba6d2
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-07-01 15:05:11 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -37,7 +37,7 @@ st.markdown(
     f"""
     <div class="container">
         <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
-        <p class="logo-text">Logo Much ?</p>
+        <p class="logo-text">Glovalth Health Tech Care Limited</p>
     </div>
     """,
     unsafe_allow_html=True
