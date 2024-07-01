e3f1b84d189e530728444a41410498b0b88e8fcc
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-07-01 18:31:01 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -50,11 +50,11 @@ st.markdown(
 
 #st.image("Glovalth_logo.jpeg", width =200)
 
-with st.sidebar:
+
 	#choice1 = option_menu("Main Menu", ["Home", "Employee Work Management Portal", "Employee Attendence Form", "Care Planning for Care Home", "Medication Activity", "Document Verification for Care Home", "Emergency", ], 
         #icons=['house', 'list-task', 'cloud-upload', 'list-task', 'list-task','list-task'], menu_icon="cast", default_index=0)
 
-	choice = option_menu("Menu", ["Home", "Employee Work Management Portal", "Task Completion Form", "Care Planning for Care Home", "Document Management Portal", "Document Sync","Medication Activity","Emergency"], 
+choice = option_menu("Menu", ["Home", "Employee Work Management Portal", "Task Completion Form", "Care Planning for Care Home", "Document Management Portal", "Document Sync","Medication Activity","Emergency"], 
         icons=['house'], menu_icon="cast",default_index=1, orientation="horizontal", 
 	styles={
         "container": {"padding": "0!important" },
