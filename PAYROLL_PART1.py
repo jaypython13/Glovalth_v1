8ad6094fbc79cddcd26c7c4179c81ffef8b0f143
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-07-01 15:09:20 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -53,7 +53,7 @@ st.markdown(
 choice = option_menu("Menu", ["Home", "Employee Work Management Portal", "Employee Attendence Form", "Care Planning for Care Home", "Medication Activity", "Document Verification for Care Home", "Emergency", ], 
         icons=['house', 'list-task', 'cloud-upload', 'list-task', 'list-task','list-task'], menu_icon="cast",default_index=1, orientation="horizontal", 
 styles={
-        "container": {"padding": "0!important", "img src" : "Glovalth_logo.jpeg", "style": "width:150px"},
+        "container": {"padding": "0!important" },
         "icon": {"color": "orange", "font-size": "20px"}, 
         "nav-link": {"font-size": "20px", "text-align": "center", "margin":"0px", "--hover-color":  "#000000"},
         "nav-link-selected": {"background-color": "green"},
