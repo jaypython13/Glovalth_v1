8fa240f5417e5872ecb1815a037c9882f1ae10eb
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-07-01 15:08:07 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -10,7 +10,7 @@ import base64
 #df2 = pd.read_csv('E012_EMP_DATA.csv',usecols = ['Employee ID','Location', 'Date','Day', 'Shift Timing','Tasks'])
 
 # Streamlit User Interface part
-st.set_page_config(page_title ="Glovalth", page_icon =":guardsman:", layout ="wide")
+st.set_page_config(page_title ="Glovalth", page_icon =":guardsman:")
 LOGO_IMAGE = "Glovalth_logo.jpeg"
 
 st.markdown(
