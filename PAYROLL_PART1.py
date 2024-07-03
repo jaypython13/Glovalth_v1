332ab8907e782b46dea37352af32740d99523d4d
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-07-03 13:15:09 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -43,7 +43,7 @@ if choice == "Task Completion Form":
 		date = st.date_input("Task Date")
 		starttime =  st.time_input("Start time", datetime.time(0,0))
 		endtime = st.time_input("End time", datetime.time(0,0))
-		Tasknotes = st.text_area (" Add your Task Notes here")
+		Tasknotes = st.text_area (" Notes Regarding Time Period")
 		submitted = st.form_submit_button("Submit")
 	
 
