6d93a02592b4235ff04b2b8349e5e587d330ecd7
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-07-03 14:03:46 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -50,6 +50,7 @@ if choice == "Task Completion Form":
 		col1.multiselect("Task Name",( "Medication Administration", "Assist with meal preparation", "Vital Signs Monitoring", "Transportation and Errands", "Housekeeping", "Wound Care", "Assistance with Medical Devices", "Mobility Assistance", "Personal Care with Medical Focus","Personal Hygiene", "Emotional Support"), placeholder = "Choose your Task activity here")
 		check.checkbox("Task Completed")
 		check.checkbox("Task Pending")
+		notes.text_area("Short Notes")
 		#time_selection = col2.multiselect('select ShiftTime ', data.ShiftTime.unique().tolist(), key='time')
 		submitted = st.form_submit_button("Submit this form")
 	
