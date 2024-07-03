67601b71a4511d02d7fe8c90b1c0dae60ff99e1a
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-07-03 13:12:43 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -41,7 +41,7 @@ if choice == "Task Completion Form":
 		Clientname = st.text_input("Client name")
 		date = st.date_input("Task Date")
 		starttime =  st.time_input("Start time", datetime.time(0,0))
-		endtime = st.time_input("End time", datetime.time(0,)))
+		endtime = st.time_input("End time", datetime.time(0,0))
 		submitted = st.form_submit_button("Submit")
 	
 
