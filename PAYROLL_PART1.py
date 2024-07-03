da9d8a0bd23f7e64bb0998ac167d081245390e1a
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-07-03 13:43:58 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -47,9 +47,9 @@ if choice == "Task Completion Form":
 		Tasknotes = st.text_area (" Notes Regarding Time Period", placeholder = " Type your notes here")
 		st.subheader(" Activity Record")
 		activity, check, notes = st.columns(3)
-		activity.st.selectbox("choose your task activity here", ("Medication Administration, "Assist with meal preparation", "Vital Signs Monitoring", 
-  			"Transportation and Errands","Housekeeping", "Wound Care", "Assistance with Medical Devices", "Mobility Assistance", "Personal Care with Medical Focus",
-     			"personal Hygiene", Emotional Support")
+		activity.st.selectbox("choose your task activity here", ("Medication Administration", "Assist with meal preparation", "Vital Signs Monitoring", 
+  			"Transportation and Errands", "Housekeeping", "Wound Care", "Assistance with Medical Devices", "Mobility Assistance", "Personal Care with Medical Focus",
+     			"personal Hygiene", "Emotional Support")
 
 		#time_selection = col2.multiselect('select ShiftTime ', data.ShiftTime.unique().tolist(), key='time')
 		submitted = st.form_submit_button("Submit this form")
