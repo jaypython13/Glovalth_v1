f6e4dbaa1497b7b86675f8b19ee3c90fda022c26
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-07-03 10:12:51 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import csv
-from st_aggrid import AgGrid
 from streamlit_option_menu import option_menu
 from streamlit_dynamic_filters import DynamicFilters
 import base64
