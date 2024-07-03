1d3bcffb86024bf63cfdd7f0bd30bf891da1f61b
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-07-03 17:12:23 +0100

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -28,7 +28,7 @@ choice = option_menu("Menu", ["Home", "Login/Sign Up", "Employee Work Management
 	styles={
         "container": {"padding": "0!important" },
         "icon": {"color": "orange", "font-size": "15px"}, 
-        "nav-link": {"font-size": "20px", "text-align": "center", "margin":"0px", "--hover-color":  "#000000"},
+        "nav-link": {"font-size": "20px", "text-align": "center", "margin":"10px", "--hover-color":  "#000000"},
         "nav-link-selected": {"background-color": "green"},
     	})
 
