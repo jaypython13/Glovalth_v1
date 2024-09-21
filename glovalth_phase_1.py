e8f8ae3d48a06f156b627160d263676ef8169cca
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-09-21 13:27:33 +0100

    Update glovalth_phase_1.py

--- glovalth_phase_1.py
+++ glovalth_phase_1.py
@@ -15,7 +15,7 @@ from PIL import Image
     #st.header("Glovalth product Demo")
     
 # Streamlit User Interface part
-st.set_page_config(page_title ="Glovalth", page_icon ="	:medical_symbol:", layout= "wide", sidebar = "fixed")
+st.set_page_config(page_title ="Glovalth", page_icon ="	:medical_symbol:", layout= "wide", sidebar = "expanded")
 st.page_link(video_bytes, label="Glovalth's prototype Demo", icon="🌎")
 img = Image.open('Glovalth_logo.png')
 st.image(img)
