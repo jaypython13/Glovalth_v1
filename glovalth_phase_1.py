9b1d62e576b15f1b64414a21d89e49538cf96ea1
jaypython13 <140917755+jaypython13@users.noreply.github.com>
2024-09-21 14:09:55 +0100

    Update glovalth_phase_1.py

--- glovalth_phase_1.py
+++ glovalth_phase_1.py
@@ -21,7 +21,7 @@ st.image(img)
 st.header(":blue[Glovalth Health Tech Care Limited]") 
 st.subheader(":green[Empowering Care Homes with Smart Software Solutions for Seamless Operations and Exceptional Resident Care]")
 #st.button("Click below to view prortoype Demo" )
-if st.button("**Click here to view Glovalth prototype Demo**",type="primary"):
+if st.button("**Click here to view Glovalth prototype Demo**",type="secondary"):
 	video_file = open("Glovalth_product_POC_V1.mp4", "rb")
 	video_bytes = video_file.read()
 	DEFAULT_WIDTH = 80
